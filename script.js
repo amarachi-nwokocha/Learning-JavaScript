225fb0566492a8c9455cae5d0e7eb3fd4f53ed7d
amarachi-nwokocha <63500921+amarachi-nwokocha@users.noreply.github.com>
2020-08-31 14:32:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function dateEnd(){
+     let today = new Date();
+     let deadline = new Date(2020, 07, 31, 11, 59, 0);
+
+     if (todaydate !== deadline ){
+         alert("submitted successfully")
+     }
+     else {
+         alert("registration closed");
+     }
+}
+
+
+
+
+
+
+
